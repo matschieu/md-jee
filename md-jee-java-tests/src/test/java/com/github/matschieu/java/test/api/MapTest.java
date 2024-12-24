c15253a143d5403a8e241266ba7b972a08c2505d
Matschieu <mathieudietsch@yahoo.fr>
2024-12-24 11:13:28 +0100

    Add test cases for Map

--- md-jee-java-tests/src/test/java/com/github/matschieu/java/test/api/MapTest.java
+++ md-jee-java-tests/src/test/java/com/github/matschieu/java/test/api/MapTest.java
@@ -8,28 +8,49 @@ import org.junit.jupiter.api.Test;
 
 class MapTest {
 
+	private static final String KEY = "KEY";
+	private static final String VALUE = "value";
+
 	@Test
 	void test() {
-		final String key = "KEY";
-		final String value = "value";
-
 		final Map<String, String> map = new HashMap<>();
 
-		Assertions.assertNull(map.get(key));
+		Assertions.assertNull(map.get(KEY));
 
-		map.computeIfAbsent(key, v -> "");
+		map.computeIfAbsent(KEY, v -> "");
 
-		Assertions.assertNotNull(map.get(key));
-		Assertions.assertEquals("", map.get(key));
+		Assertions.assertNotNull(map.get(KEY));
+		Assertions.assertEquals("", map.get(KEY));
 
-		map.put(key, value);
+		map.put(KEY, VALUE);
 
-		Assertions.assertNotNull(map.get(key));
-		Assertions.assertEquals(value, map.get(key));
+		Assertions.assertNotNull(map.get(KEY));
+		Assertions.assertEquals(VALUE, map.get(KEY));
 
-		map.computeIfAbsent(key, v -> "");
+		map.computeIfAbsent(KEY, v -> "");
 
-		Assertions.assertNotNull(map.get(key));
-		Assertions.assertEquals(value, map.get(key));
+		Assertions.assertNotNull(map.get(KEY));
+		Assertions.assertEquals(VALUE, map.get(KEY));
+	}
+
+	private int count = 0;
+
+	@Test
+	void testComputeIfAbsent() {
+		final Map<String, String> map = new HashMap<>();
+
+		Assertions.assertNull(map.get(KEY));
+
+		for(int i = 0; i < 10; i++) {
+			map.computeIfAbsent(KEY, k -> {
+				count++;
+				System.out.println("Add value to map");
+				return VALUE;
+			});
+		}
+
+		Assertions.assertEquals(1, map.size());
+		Assertions.assertEquals(1, count);
+		Assertions.assertEquals(VALUE, map.get(KEY));
 	}
 }
